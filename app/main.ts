23fe672991bd18790e579f8ff227f111e4789205
Ayush <ayush1404@gmail.com>
2024-07-29 01:14:02 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -7,6 +7,11 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.on('data',(data)=>{
         console.log(data)
+        const request =  data.toString();
+        const path = request.split(' ')[1]
+        const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`
+        socket.write(Buffer.from(response))
+        socket.end()
     })
     // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
     // socket.end();
