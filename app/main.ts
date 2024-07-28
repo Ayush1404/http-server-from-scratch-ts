27cb2ef08dc2760a339a6ed37da9e845eef9764a
Ayush <ayush1404@gmail.com>
2024-07-29 00:46:52 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -7,6 +7,9 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
     socket.end();
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
