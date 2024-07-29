fc2abfb8e43f8301bd120b0a5eb10bfb58a76274
Ayush <ayush1404@gmail.com>
2024-07-29 23:21:39 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -8,12 +8,28 @@ const server = net.createServer((socket) => {
     socket.on('data',(data)=>{
         const request =  data.toString();
         const path = request.split(' ')[1]
-        const isEcho = path.split('/')[1] === 'echo'
-        if(isEcho)
-        {const str = path.split('/')[2]
-        const response = isEcho ? `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}` : `HTTP/1.1 404 Not Found\r\n\r\n`
-        socket.write(Buffer.from(response))
-        socket.end()}
+        const route =  path.split('/')[1]
+
+        if(route === 'user-agent')
+        {
+            const headers = request.split('\r\n')
+            headers.forEach((header)=>{
+                if(header.split(':')[0] === 'User-Agent')
+                {
+                    const response =`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${header.split(':')[1].length-1}\r\n\r\n${header.split(':')[1].substring(1)}`
+                    socket.write(Buffer.from(response))
+                    socket.end()
+                }
+            })
+        }
+        
+        if(route === 'echo')
+        {
+            const str = path.split('/')[2]
+            const response =`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`
+            socket.write(Buffer.from(response))
+            socket.end()
+        }
         const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`
         socket.write(Buffer.from(response))
         socket.end()
