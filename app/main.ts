86c298dc2a3a2048a529cba853571f94317e5d3b
Ayush <ayush1404@gmail.com>
2024-07-29 23:34:45 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,44 +1,46 @@
-import * as net from "net";
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-//Uncomment this to pass the first stage
+import * as net from 'node:net';
 const server = net.createServer((socket) => {
-    socket.on('data',(data)=>{
-        const request =  data.toString();
-        const path = request.split(' ')[1]
-        const route =  path.split('/')[1]
-
-        if(route === 'user-agent')
-        {
-            const headers = request.split('\r\n')
-            headers.forEach((header)=>{
-                if(header.split(':')[0] === 'User-Agent')
-                {
-                    const response =`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${header.split(':')[1].length-1}\r\n\r\n${header.split(':')[1].substring(1)}`
-                    socket.write(Buffer.from(response))
-                    socket.end()
-                }
-            })
+    socket.on('data', (data) => {
+        const request = data.toString();
+        console.log(request);
+        const path = request.split(' ')[1];
+        console.log(path.split('/')[1])
+        const params = path.split('/')[1];
+        let response: string;
+        function changeResponse(response: string): void {
+            socket.write(response);
+            socket.end();
         }
-        
-        if(route === 'echo')
-        {
-            const str = path.split('/')[2]
-            const response =`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`
-            socket.write(Buffer.from(response))
-            socket.end()
+        switch (params) {
+            case '': {
+                response = 'HTTP/1.1 200 OK\r\n\r\n'
+                changeResponse(response)
+                break;
+            }
+            case 'echo': {
+                const message = path.split('/')[2]
+                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
+                changeResponse(response)
+                break;
+            }
+            case 'user-agent': {
+                const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
+                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+                changeResponse(response)
+                break;
+            }
+            default: {
+                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+                changeResponse(response)
+                break;
+            }
         }
-        const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`
-        socket.write(Buffer.from(response))
         socket.end()
     })
-    // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-    // socket.end();
-    socket.on("close", () => {
-        socket.end();
-    });
 });
-
-server.listen(4221, "localhost");
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+// Uncomment this to pass the first stage
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
+});
