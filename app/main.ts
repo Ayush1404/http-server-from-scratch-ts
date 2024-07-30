befa75decce640f97cded7b50dc2622363229564
Ayush <ayush1404@gmail.com>
2024-07-30 11:47:39 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,7 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
         console.log(request);
+        const method = request.split(' ')[0]
         const path = request.split(' ')[1];
         console.log(path.split('/')[1])
         const params = path.split('/')[1];
@@ -13,57 +14,88 @@ const server = net.createServer((socket) => {
             socket.write(response);
             socket.end();
         }
-        switch (params) {
-            case '': {
-                response = 'HTTP/1.1 200 OK\r\n\r\n'
-                changeResponse(response)
-                break;
+        switch(method) {
+            case 'POST' : {
+                switch(params){
+                    case 'files':{
+                        const fileName = path.split('/')[2]
+                        console.log(process.argv)
+                        const args = process.argv.slice(3)
+                        console.log(args)
+                        const filePath = pathlib.join(args[0], fileName);
+                        const req = request.split('\r\n')
+                        const content = req[req.length - 1]
+                        console.log(filePath)
+                        console.log(fileName)
+                        console.log(content)
+                        try{
+                            const data = fs.writeFileSync(filePath,content)
+                            console.log(data)
+                            response = `HTTP/1.1 201 Created\r\n\r\n`
+                            changeResponse(response)
+                        }catch(err:any)
+                        {
+                            console.log(err)
+                        }
+                    }
+                        
+                }
             }
-            case 'files': {
-                const fileName = path.split('/')[2]
-                console.log(process.argv)
-                const args = process.argv.slice(3)
-                console.log(args)
-                const filePath = pathlib.join(args[0], fileName);
-                console.log(filePath)
-                console.log(fileName)
-                let response:string;
-                if(fs.existsSync(filePath))
-                {
-                    try{
-                        const data = fs.readFileSync(filePath) 
-                        console.log(data)
-                        response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`
+            case 'GET' :{
+                switch (params) {
+                    case '': {
+                        response = 'HTTP/1.1 200 OK\r\n\r\n'
                         changeResponse(response)
-                    }catch(err:any)
-                    {
-                        console.log(err)
+                        break;
+                    }
+                    case 'files': {
+                        const fileName = path.split('/')[2]
+                        console.log(process.argv)
+                        const args = process.argv.slice(3)
+                        console.log(args)
+                        const filePath = pathlib.join(args[0], fileName);
+                        console.log(filePath)
+                        console.log(fileName)
+                        let response:string;
+                        if(fs.existsSync(filePath))
+                        {
+                            try{
+                                const data = fs.readFileSync(filePath) 
+                                console.log(data)
+                                response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`
+                                changeResponse(response)
+                            }catch(err:any)
+                            {
+                                console.log(err)
+                            }
+                        }
+                        else{
+                            response = `HTTP/1.1 404 Not Found\r\n\r\n`
+                            changeResponse(response)
+                        }
+                        
+                    }
+                    case 'echo': {
+                        const message = path.split('/')[2]
+                        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
+                        changeResponse(response)
+                        break;
+                    }
+                    case 'user-agent': {
+                        const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
+                        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+                        changeResponse(response)
+                        break;
+                    }
+                    default: {
+                        response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+                        changeResponse(response)
+                        break;
                     }
                 }
-                else{
-                    response = `HTTP/1.1 404 Not Found\r\n\r\n`
-                    changeResponse(response)
-                }
-                
-            }
-            case 'echo': {
-                const message = path.split('/')[2]
-                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
-                changeResponse(response)
-                break;
-            }
-            case 'user-agent': {
-                const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
-                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-                changeResponse(response)
-                break;
-            }
-            default: {
-                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
-                changeResponse(response)
-                break;
             }
         }
+        
         socket.end()
     })
 });
